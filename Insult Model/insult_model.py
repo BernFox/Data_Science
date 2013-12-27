f0217495d0cb61315c68c7fc9e1c716dffe10223
ABernkopf <agbernkopf@gmail.com>
2013-12-27 01:08:45 -0500

    Updated code
    insult_model cleaned up and updated

--- Insult Model/insult_model.py
+++ Insult Model/insult_model.py
@@ -37,6 +37,7 @@ def binary_time():
 		1 if the time is anything else. If the cell doesn't contain a time, it is binned as -1.
 		 Leaving any bins empty would make classification on the test set have to exclude 
 		 empyt time cells. This is done for both the training and test set"""
+
 	for i, date in enumerate(f_train['Date']):
 		try:
 			if 0 <= int(date[-5:-3]) < 7:
@@ -60,6 +61,7 @@ def binary_time():
 def FourBinTime():
 	"""This bins the time into 4 bins by grabbing the hour and dividing by 6. Empty time cells are given -1.
 		This is done for both the training and test set"""
+
 	for i, date in enumerate(f_train['Date']):
 		try:
 			f_train['tbin'].ix[i] = int(date[-5:-3])/6
@@ -76,6 +78,8 @@ def FourBinTime():
 def format_time_train():
 	"""format the time column by getting rid of the last character 'Z', and then parsing the time using the dateutil parser
 		then write out to a csv"""
+
+	print 'Formatting the training set...', '\n'
 	for i in xrange( len( in_train['Date'] )):
 		try: 
 			in_train['Date'].ix[i] = parser.parse( in_train['Date'].ix[i][:-1] )
@@ -86,6 +90,8 @@ def format_time_train():
 
 def format_time_test():
 	"""this function does the same thing as format_time_train, but does it for the test data"""
+
+	print 'Formatting the test set...', '\n'
 	for j in xrange( len( in_test['Date'] )):
 		try:
 			in_test['Date'].ix[j] = parser.parse( in_test['Date'].ix[j][:-1] )
@@ -95,7 +101,7 @@ def format_time_test():
 	in_test.to_csv(out_test_data)
 
 
-def class_insult(mini=2 ,maxi=50, folds=10, nmin=1, nmax=2, bin='b'):
+def class_insult(mini=2 ,maxi=50, folds=10, nmin=1, nmax=2, bin=4):
 	"""This function extracts text features from a series of insults (identified as such), 
 		then tries to classify a document that does not have labels. 
 
@@ -115,6 +121,7 @@ def class_insult(mini=2 ,maxi=50, folds=10, nmin=1, nmax=2, bin='b'):
 		FourBinTime()
 		print 'FourBinTime', '\n'
 
+	print 'Analyzing...', '\n'
 	i_train = f_train[f_train['Insult'].copy() ==1]
 
 	stopwords = ['is','Is','was','to','it','It','To','an','An','And','and','a','A','Are','are','What','what','for','For','From','from','When','when','can','Can','This','this']
@@ -136,12 +143,13 @@ def class_insult(mini=2 ,maxi=50, folds=10, nmin=1, nmax=2, bin='b'):
 	vectorizer1 = CountVectorizer(min_df=mini, max_df=maxi, decode_error='ignore',stop_words=stopwords, 
 		vocabulary=tvocab, ngram_range=(nmin,nmax))
 
+	#Turn the fitted CountVectorizer into a numeric matrix, then into a dataframe, then concatenate with the binned time column
 	X = vectorizer1.fit_transform(f_train['Comment'])
 	X0 = pd.DataFrame( X.toarray() )
 	X1 = pd.concat([pd.DataFrame(f_train['tbin']),X0], axis=1)
 
 
-	all_fpr, all_tpr, all_auc, all_acc = (np.zeros(folds), np.zeros(folds), np.zeros(folds),np.zeros(folds))
+	all_acc = np.zeros(folds)
 
 	for i,(traini, testi) in enumerate(kf):
 
@@ -158,10 +166,12 @@ def class_insult(mini=2 ,maxi=50, folds=10, nmin=1, nmax=2, bin='b'):
 
 		gnb.fit(train_features,train_labels)
 		predicted_labels = gnb.predict(test_features)
+		acc = gnb.score(test_features, test_labels)
+
+		#Print some analysis of the classifier
 		print predicted_labels, '\n'
 		print "The number of insults predicted =",predicted_labels.sum()
 
-		acc = gnb.score(test_features, test_labels)
 		print "Accuracy =",acc
 		print confusion_matrix(test_labels, predicted_labels)
 		print classification_report(test_labels,predicted_labels, [0,1])
@@ -171,23 +181,39 @@ def class_insult(mini=2 ,maxi=50, folds=10, nmin=1, nmax=2, bin='b'):
 
 	print '\n',"Average accuracy =",np.mean(all_acc)
 
-	vectorizer2 = CountVectorizer(min_df=mini, max_df=maxi, decode_error='ignore',stop_words=stopwords, vocabulary=tvocab, ngram_range=(nmin,nmax))
+	#Initialize a new CountVectorizer using the vocabulary learned from the insult comments in the training set
+	vectorizer2 = CountVectorizer(min_df=mini, max_df=maxi, decode_error='ignore',
+		stop_words=stopwords, vocabulary=tvocab, ngram_range=(nmin,nmax))
+
+	#Fit the CountVectorizer with the comments from the test set, then connect with the binned time column
 	result = vectorizer2.fit_transform(f_test['Comment'])
 	result_array = pd.DataFrame( result.toarray() )
 	result_array1 = pd.concat([pd.DataFrame(f_test['tbin']),result_array], axis=1)
 
+	#Initialize and fit 
 	gnb1 = GaussianNB()
 	gnb1.fit(X1,labels)
 
+	#Predict labels for test set comments, then connect predictions with test dataframe and rename the column from it's default name
 	predicted_results = gnb1.predict(result_array1)
-
 	predicted = pd.concat( [pd.DataFrame(predicted_results),f_test], axis=1 )
-
 	predicted = predicted.rename(columns={0 : 'Insult'})
 
+	#Print some dataframe with predictions to test results
 	print predicted.ix[0:15]  
 
+	#Output result dataframe as csv
 	predicted.to_csv('predicted_results.csv')
+
+
+if __name__ == '__main__':
+	format_time_train()
+	format_time_test()
+	class_insult()
+
+
+
+
 
 
 
